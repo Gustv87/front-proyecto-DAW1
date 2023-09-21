395f270c34d3ad883ffc8710a82e8550ebd1de9e
gustavomartinez <gustavomartinezh87@gmail.com>
2023-09-21 00:59:03 -0600

    fix del nav bar

--- front-proyecto-DAW1/src/components/menu/Menu.jsx
+++ front-proyecto-DAW1/src/components/menu/Menu.jsx
@@ -1,45 +1,58 @@
-import { useState } from 'react';
 import './Menu.css'
 
-
 const Menu = () => {
-
-    const [collapsed, setCollapsed] = useState(false);
-
-    const toggleSidebar = () => {
-        setCollapsed(!collapsed);
-    };
 
     return (
         <>
 
             {/* <!-- Menu --> */}
-            <nav id="sidebar" className={collapsed ? 'active' : ''}>
-                <div className="sidebar-header">
-                    <h3>Nombre de la Aplicación</h3>
+
+            <div className="d-flex">
+                <div className="sidebar-container ">
+                    <div className="logo">
+                        <h4 className='text-light font-weight-bold'>Reservacion de Laboratorios</h4>
+                    </div>
+                    <div className="menu">
+                        <a href="#" className='d-block text-light p-3'><i className="bi bi-person me-2 lead"></i>Usuarios</a>
+                        <a href="#" className='d-block text-light p-3'><i className="bi bi-book me-2 lead"></i>Reservaciones</a>
+                        <a href="#" className='d-block text-light p-3'><i className="bi bi-buildings me-2 lead"></i>Laboratorios</a>
+                        <a href="#" className='d-block text-light p-3'><i className="bi bi-calendar-event me-2 lead"></i>Horarios</a>
+                    </div>
                 </div>
-
-                <ul className="list-unstyled components">
-                    <p>Menú</p>
-                    <li>
-                        <a href="#">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="#">Productos</a>
-                    </li>
-                    <li>
-                        <a href="#">Servicios</a>
-                    </li>
-                    <li>
-                        <a href="#">Contacto</a>
-                    </li>
-                </ul>
-
-                <button className="btn btn-secondary toggle-btn" onClick={toggleSidebar}>
-                    {collapsed ? 'Abrir' : 'Cerrar'} Menú
-                </button>
-            </nav>
-
+                <div className='w-100'>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+                </div>
+            </div>
 
 
         </>
