41b44d4be63c9337282ed703c29c0eb7859691ae
gustavomartinez <gustavomartinezh87@gmail.com>
2023-09-21 01:21:58 -0600

    fix

--- front-proyecto-DAW1/src/main.jsx
+++ front-proyecto-DAW1/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import * as bootstrap from 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
